e7dcdeb424fe8d32a0ff2a19a8b9b318f6cc86b0
Judy Tan <judytan86@gmail.com>
2019-06-10 14:22:34 -0400

    layout

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Menu from './Components/Menu/Menu'
+import Search from "./Components/Searchbar/Search";
+import Total from "./Components/Total/Total";
+import ViewTabs from "./Components/Tabs/ViewsTab";
+import Portfolio from "./Components/Portfolio/Portfolio";
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <Menu />
+      <Search />
+      <ViewTabs /> 
+      <Total />
+      <Portfolio />
+    </div>
+    )
+  }
+}
+
+export default App;
